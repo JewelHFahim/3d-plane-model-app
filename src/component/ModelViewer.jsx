1fbd15f28c3820821adeb83e32bfd6907179eb6e
Jewel Hossain Fahim <76914444+JewelHFahim@users.noreply.github.com>
2023-06-15 13:11:15 +0600

    left and right swing

--- src/component/ModelViewer.jsx
+++ src/component/ModelViewer.jsx
@@ -1,28 +1,48 @@
 import { OrbitControls } from '@react-three/drei';
-import React, { startTransition, useEffect } from 'react';
-import { Canvas, useLoader } from 'react-three-fiber';
+import React, { useEffect, useRef } from 'react';
+import { Canvas, useLoader, useFrame } from 'react-three-fiber';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
-function  ModelViewer() {
+function ModelViewer() {
   const gltf = useLoader(GLTFLoader, './models/plane.glb');
+  const modelRef = useRef();
+  const swingSpeed = 0.1; // Adjust the swing speed as desired
 
+  let isSwingingRight = true;
 
-  useEffect(() => {
-    startTransition(() => {
-      // Existing code within the useEffect callback
-    });
-  }, []);
-
-
+  useFrame((state, delta) => {
+    if (modelRef.current) {
+      // Update the rotation of the model for swinging animation
+      if (isSwingingRight) {
+        modelRef.current.rotation.z += swingSpeed * delta;
+        if (modelRef.current.rotation.z >= Math.PI / 8) {
+          isSwingingRight = false;
+        }
+      } else {
+        modelRef.current.rotation.z -= swingSpeed * delta;
+        if (modelRef.current.rotation.z <= -Math.PI / 8) {
+          isSwingingRight = true;
+        }
+      }
+    }
+  });
 
   return (
+    <mesh ref={modelRef} castShadow receiveShadow>
+      <primitive object={gltf.scene} />
+    </mesh>
+  );
+}
+
+function App() {
+  return (
     <Canvas className='test'>
-      <OrbitControls />
       <ambientLight intensity={0.5} />
       <pointLight position={[10, 10, 10]} />
-      {gltf && <primitive object={gltf.scene} />}
+      <OrbitControls />
+      <ModelViewer />
     </Canvas>
   );
 }
 
-export default ModelViewer;
+export default App;
